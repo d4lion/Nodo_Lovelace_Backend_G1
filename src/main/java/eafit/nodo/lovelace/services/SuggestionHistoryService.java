d4afaa29fe5defc5900f41f9cfdd06bab2cddfec
SantiagoBA98 <santyboal2@gmail.com>
2024-11-26 18:30:51 -0500

    added endpoint history by id (#7)

--- src/main/java/eafit/nodo/lovelace/services/SuggestionHistoryService.java
+++ src/main/java/eafit/nodo/lovelace/services/SuggestionHistoryService.java
@@ -1,7 +1,16 @@
 package eafit.nodo.lovelace.services;
 
+
+import eafit.nodo.lovelace.dtos.SuggestionHistoryDto;
+import eafit.nodo.lovelace.dtos.UserDataDto;
+import eafit.nodo.lovelace.entities.SuggestionHistory;
 import eafit.nodo.lovelace.entities.Suggestions;
+import eafit.nodo.lovelace.utils.ApiResponse;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface SuggestionHistoryService {
     void insertSuggestion(Suggestions suggestionHistory, Long id_user);
+    ApiResponse<SuggestionHistoryDto> getSuggestionHistoryById(UUID id);
 }
