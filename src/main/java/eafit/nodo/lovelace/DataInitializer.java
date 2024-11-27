a9d3704e4f29aef7b2d0307b77d13f899b40c1b7
d4lion <d4lion.dev@gmail.com>
2024-11-27 01:56:48 -0500

    :construction: update: Add s3 links for images

--- src/main/java/eafit/nodo/lovelace/DataInitializer.java
+++ src/main/java/eafit/nodo/lovelace/DataInitializer.java
@@ -40,41 +40,58 @@ public class DataInitializer implements CommandLineRunner {
                 "(1, 'jhondoe@mail.com', 'Doe', 'Jhon', '2024-01-15')";
 
         String CountriesDataSql = "INSERT INTO countries (id, city, continent, name, photos_url) VALUES " +
-                "(1, 'Playa del Carmen', 'América', 'México', 'url_foto_mexico'), " +
-                "(2, 'Cartagena', 'América', 'Colombia', 'url_foto_colombia'), " +
-                "(3, 'Rio de Janeiro', 'América', 'Brasil', 'url_foto_brasil'), " +
-                "(4, 'Bariloche', 'América', 'Argentina', 'url_foto_argentina'), " +
-                "(5, 'Cusco', 'América', 'Perú', 'url_foto_peru'), " +
-                "(6, 'Banff', 'América', 'Canadá', 'url_foto_canada'), " +
-                "(7, 'Nueva York', 'América', 'EE.UU.', 'url_foto_usa'), " +
-                "(8, 'Miami', 'América', 'EE.UU.', 'url_foto_usa'), " +
-                "(9, 'Toronto', 'América', 'Canadá', 'url_foto_canada'), " +
-                "(10, 'Tulum', 'América', 'México', 'url_foto_mexico'), " +
-                "(11, 'Ushuaia', 'América', 'Argentina', 'url_foto_argentina'), " +
-                "(12, 'Punta Cana', 'América', 'República Dominicana', 'url_foto_rd'), " +
-                "(13, 'San Juan', 'América', 'Puerto Rico', 'url_foto_pr'), " +
-                "(14, 'Chicago', 'América', 'EE.UU.', 'url_foto_usa'), " +
-                "(15, 'Los Ángeles', 'América', 'EE.UU.', 'url_foto_usa'), " +
-                "(16, 'Honolulu', 'América', 'Hawái', 'url_foto_hawaii'), " +
-                "(17, 'Aspen', 'América', 'EE.UU.', 'url_foto_usa'), " +
-                "(18, 'Ciudad de México', 'América', 'México', 'url_foto_mexico'), " +
-                "(19, 'Santorini', 'Europa', 'Grecia', 'url_foto_grecia'), " +
-                "(20, 'Barcelona', 'Europa', 'España', 'url_foto_espania'), " +
-                "(21, 'Lisboa', 'Europa', 'Portugal', 'url_foto_portugal'), " +
-                "(22, 'Interlaken', 'Europa', 'Suiza', 'url_foto_suiza'), " +
-                "(23, 'Granada', 'Europa', 'España', 'url_foto_espania'), " +
-                "(24, 'Zermatt', 'Europa', 'Suiza', 'url_foto_suiza'), " +
-                "(25, 'París', 'Europa', 'Francia', 'url_foto_francia'), " +
-                "(26, 'Viena', 'Europa', 'Austria', 'url_foto_austria'), " +
-                "(27, 'Berlín', 'Europa', 'Alemania', 'url_foto_alemania'), " +
-                "(28, 'Ibiza', 'Europa', 'España', 'url_foto_espania'), " +
-                "(29, 'Reykjavik', 'Europa', 'Islandia', 'url_foto_islandia'), " +
-                "(30, 'Algarve', 'Europa', 'Portugal', 'url_foto_portugal'), " +
-                "(31, 'Londres', 'Europa', 'Reino Unido', 'url_foto_reinounido'), " +
-                "(32, 'Niza', 'Europa', 'Francia', 'url_foto_francia'), " +
-                "(33, 'Chamonix', 'Europa', 'Francia', 'url_foto_francia'), " +
-                "(34, 'Roma', 'Europa', 'Italia', 'url_foto_italia'), " +
-                "(35, 'Malta', 'Europa', 'Malta', 'url_foto_malta');";
+                "(1, 'Playa del Carmen', 'América', 'México', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/mex_playa_del_carmen.jpg'), " +
+                "(2, 'Cartagena', 'América', 'Colombia', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/america/col_cartagena.jpg'), " +
+                "(3, 'Rio de Janeiro', 'América', 'Brasil', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/br_rio_de_janeiro.jpg'), " +
+                "(4, 'Bariloche', 'América', 'Argentina', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/ar_bariloche.jpg'), " +
+                "(5, 'Cusco', 'América', 'Perú', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/per_cusco.jpg'), " +
+                "(6, 'Banff', 'América', 'Canadá', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/canada_banff.jpg'), " +
+                "(7, 'Nueva York', 'América', 'EE.UU.', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/eu_new_york.jpg'), " +
+                "(8, 'Miami', 'América', 'EE.UU.', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/eu_miami.jpg'), " +
+                "(9, 'Toronto', 'América', 'Canadá', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/canada_toronto.jpg'), " +
+                "(10, 'Tulum', 'América', 'México', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/mex_tulum.jpg'), " +
+                "(11, 'Ushuaia', 'América', 'Argentina', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/ar_ushuaia.jpg'), " +
+                "(12, 'Punta Cana', 'América', 'República Dominicana', 'https://lovelace-amadeus.s3.us-east-1" +
+                ".amazonaws.com/country_img/america/rep_dominicana_punta_cana.jpg'), " +
+                "(13, 'San Juan', 'América', 'Puerto Rico', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/pr_san_juan.jpg'), " +
+                "(14, 'Chicago', 'América', 'EE.UU.', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/eu_chicago.jpg'), " +
+                "(15, 'Los Ángeles', 'América', 'EE.UU.', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/eu_los_angeles.jpg'), " +
+                "(16, 'Honolulu', 'América', 'Hawái', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/hawai_honolulu.jpg'), " +
+                "(17, 'Aspen', 'América', 'EE.UU.', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/eu_aspen.jpg'), " +
+                "(18, 'Ciudad de México', 'América', 'México', 'https://lovelace-amadeus.s3.us-east-1.amazonaws" +
+                ".com/country_img/america/mx_cdmx.jpg'), " +
+                "(19, 'Santorini', 'Europa', 'Grecia', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/grecia_santorini.jpg'), " +
+                "(20, 'Barcelona', 'Europa', 'España', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/es_barcelona.jpg'), " +
+                "(21, 'Lisboa', 'Europa', 'Portugal', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/portugal_lisboa.jpg'), " +
+                "(22, 'Interlaken', 'Europa', 'Suiza', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/suiza_interlaken.jpg'), " +
+                "(23, 'Granada', 'Europa', 'España', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/es_granada.jpg'), " +
+                "(24, 'Zermatt', 'Europa', 'Suiza', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/suiza_zermatt.jpg'), " +
+                "(25, 'París', 'Europa', 'Francia', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/alemania_berlin.jpg'), " +
+                "(26, 'Viena', 'Europa', 'Austria', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/austria_viena.jpg'), " +
+                "(27, 'Berlín', 'Europa', 'Alemania', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/alemania_berlin.jpg'), " +
+                "(28, 'Ibiza', 'Europa', 'España', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/es_ibiza.jpg'), " +
+                "(29, 'Reykjavik', 'Europa', 'Islandia', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/islandia_reykjavik.jpg'), " +
+                "(30, 'Algarve', 'Europa', 'Portugal', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/portugal_algarve.jpg'), " +
+                "(31, 'Londres', 'Europa', 'Reino Unido', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/reino_unido_london.jpg'), " +
+                "(32, 'Niza', 'Europa', 'Francia', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/francia_niza.jpg'), " +
+                "(33, 'Chamonix', 'Europa', 'Francia', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/francia_chamonix.jpg'), " +
+                "(34, 'Roma', 'Europa', 'Italia', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/italia_roma.jpg'), " +
+                "(35, 'Malta', 'Europa', 'Malta', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/malta_malta.jpg'), " +
+                "(36, 'Bora Bora', 'Oceanía', 'Polynesia', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/polynesia_bora_bora.jpg'), " +
+                "(37, 'Dubái', 'Asia', 'Emiratos Árabes Unidos', 'https://lovelace-amadeus.s3.us-east-1.amazonaws.com/country_img/europa/emiratos_dubai.jpg');";
+
 
         String SuggestionDataSql = "INSERT INTO suggestions (id, climate, activity, housing, duration, age, " +
                 "america_country_id, europa_country_id) VALUES " +
@@ -96,7 +113,9 @@ public class DataInitializer implements CommandLineRunner {
                 "(16, 'Caluroso', 'Cultura y museos', 'Hotel de lujo', 'Una a dos semanas', 'Más de 50 años', 15, 34), " +
                 "(17, 'Caluroso', 'Cultura y museos', 'Hotel de lujo', 'Una a dos semanas', '30-50 años', 16, 35), " +
                 "(18, 'Frío', 'Relax y bienestar', 'Airbnb o apartamento', 'Una a dos semanas', 'Más de 50 años', 17, 26), " +
-                "(19, 'Templado', 'Cultura y museos', 'Albergue', 'Una a dos semanas', '30-50 años', 18, 20);";
+                "(19, 'Templado', 'Cultura y museos', 'Albergue', 'Una a dos semanas', '30-50 años', 18, 20), " +
+                "(20, 'default', 'default', 'default', 'default', 'default', 36, 37);";
+
 
         entityManager.createNativeQuery(CountriesDataSql).executeUpdate();
         entityManager.createNativeQuery(UsersDataSql).executeUpdate();
